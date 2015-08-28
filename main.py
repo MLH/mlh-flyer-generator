3578df9d926165c3096ee69df9a65afb178d0c7c
Shyamal Ruparel <Shyamal@Ruparel.co>
2015-08-28 20:27:06 +0100

    Made it so generates based on a filepath.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw,ImageFont
+import csv
 
 im = Image.open('base.png').convert('RGBA')
 W, H = im.size
@@ -6,37 +7,42 @@ W, H = im.size
 MaxSize = 200
 maxFontW = W * .66666666
 
-venueName = "University of Texas"
-addressDetails = "2317 Speedway, Austin, TX 78712"
+with open('data.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        im = Image.open('base.png').convert('RGBA')
 
-venueSize = MaxSize
-addressSize = MaxSize/2
+        venueSize = MaxSize
+        addressSize = MaxSize/2
 
-venue = ImageFont.truetype('fonts/Outage.ttf', venueSize)
-address = ImageFont.truetype('fonts/OpenSansRegular.ttf', 100)
+        venueName = row[0]
+        addressDetails = row[1]
 
+        venue = ImageFont.truetype('fonts/Outage.ttf', venueSize)
+        address = ImageFont.truetype('fonts/OpenSansRegular.ttf', addressSize)
 
-draw = ImageDraw.Draw(im)
+        draw = ImageDraw.Draw(im)
 
-wVenue, hVenue = draw.textsize(venueName,font=venue)
+        wVenue, hVenue = draw.textsize(venueName,font=venue)
 
-while (wVenue > maxFontW):
-    print venueSize
-    venueSize = venueSize - 10
-    venue = ImageFont.truetype('fonts/Outage.ttf', venueSize)
-    wVenue, hVenue = draw.textsize(venueName,font=venue)
+        while (wVenue > maxFontW):
+            venueSize = venueSize - 10
+            venue = ImageFont.truetype('fonts/Outage.ttf', venueSize)
+            wVenue, hVenue = draw.textsize(venueName,font=venue)
 
-wAddress, hAddress = draw.textsize(addressDetails,font=address)
+        wAddress, hAddress = draw.textsize(addressDetails,font=address)
 
-while (wAddress > maxFontW):
-    print addressSize
-    addressSize = addressSize - 10
-    address = ImageFont.truetype('fonts/OpenSansRegular.ttf', addressSize)
-    wAddress, hAddress = draw.textsize(addressDetails,font=address)
-
-
-draw.text(((W-wVenue)/2,(H-hVenue)/2), venueName,font=venue, fill="white")
-draw.text(((W-wAddress)/2,((H-hAddress)/2)+hVenue+10), addressDetails,font=address, fill="white")
+        while (wAddress > maxFontW):
+            addressSize = addressSize - 10
+            address = ImageFont.truetype('fonts/OpenSansRegular.ttf', addressSize)
+            wAddress, hAddress = draw.textsize(addressDetails,font=address)
 
 
-im.show()
+        draw.text(((W-wVenue)/2,(H-hVenue)/2), venueName,font=venue, fill="white")
+        draw.text(((W-wAddress)/2,((H-hAddress)/2)+hVenue+10), addressDetails,font=address, fill="white")
+
+        filename = 'output/' + venueName.strip() + '.png'
+        filename = filename.replace (" ", "_")
+        print filename
+        im.save(filename,'PNG')
+# im.show()
